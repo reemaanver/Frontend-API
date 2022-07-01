f10411641fd68a4e1c616162bf0cb4e94b8d1ad3
reemaanver <108041797+reemaanver@users.noreply.github.com>
2022-07-01 07:23:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <p>hehe</p>
+    </div>
+  );
+}
+
+export default App;
